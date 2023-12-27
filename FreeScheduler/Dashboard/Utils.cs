be27604419115ec47020daeac201f9ff376d4411
2881099 <2881099@qq.com>
2023-12-27 14:24:25 +0800

    v2.0.19 #2

--- FreeScheduler/Dashboard/Utils.cs
+++ FreeScheduler/Dashboard/Utils.cs
@@ -50,15 +50,15 @@ namespace FreeScheduler.Dashboard
             public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
             {
 				return null;
-                if (reader.TokenType != JsonToken.Integer)
-                    throw new Exception($"Unexpected token parsing date. Expected Integer, got {reader.TokenType}.");
+                //if (reader.TokenType != JsonToken.Integer)
+                //    throw new Exception($"Unexpected token parsing date. Expected Integer, got {reader.TokenType}.");
 
-                var ticks = (long)reader.Value;
+                //var ticks = (long)reader.Value;
 
-                var date = new DateTime(1970, 1, 1);
-                date = date.AddSeconds(ticks);
+                //var date = new DateTime(1970, 1, 1);
+                //date = date.AddSeconds(ticks);
 
-                return date;
+                //return date;
             }
 
             public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
@@ -67,11 +67,11 @@ namespace FreeScheduler.Dashboard
                 {
 					writer.WriteValue(((DateTime)value).ToString("yyyy-MM-dd HH:mm:ss"));
 					return;
-                    var epoc = new DateTime(1970, 1, 1);
-                    var delta = ((DateTime)value) - epoc;
-                    if (delta.TotalSeconds < 0)
-                        throw new ArgumentOutOfRangeException("Unix epoc starts January 1st, 1970");
-					writer.WriteValue((long)delta.TotalSeconds);
+     //               var epoc = new DateTime(1970, 1, 1);
+     //               var delta = ((DateTime)value) - epoc;
+     //               if (delta.TotalSeconds < 0)
+     //                   throw new ArgumentOutOfRangeException("Unix epoc starts January 1st, 1970");
+					//writer.WriteValue((long)delta.TotalSeconds);
                 }
                 else
                     throw new Exception("Expected date object value.");
